4a772dcc0aabcf66540252e0339ed6bf4b694a1d
Gaspar Soto <gaspar.soto@gmail.com>
2021-01-12 22:30:31 -0300

    Set cors header in response

--- controller/controller.ts
+++ controller/controller.ts
@@ -3,6 +3,7 @@ import { Router } from "https://deno.land/x/oak/mod.ts";
 export const makeHelloWorld = (router: Router, service: any) => {
   return () => {
     router.get("/", (ctx) => {
+      ctx.response.headers.set("Access-Control-Allow-Origin", "*");
       ctx.response.body = service.getHelloWorld();
     });
   };
